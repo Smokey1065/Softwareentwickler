f40ad7006204579a004bff478fe59cee552bd24c
Smokey1065 <hessorogan@gmail.com>
2023-12-14 18:44:50 +0100

    Update

---
+++ Javascript/script.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", function () {
+    // Verstecke den Preloader
+    document.querySelector(".preloader").style.display = "none";
+    // Zeige den eigentlichen Inhalt
+    document.querySelector(".content").style.display = "block";
+  });
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
+
+  function danke(){
+    alert('Danke das du mein Newsletter Aboniert Hast!');
+  }
